63585f496bd266073daa0bd8ec04f3c35ca39577
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-21 14:49:38 -0800

    display weather

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const apikey = "11dc52a95fa797bda83ead455ac98597";
+const apiKey = "11dc52a95fa797bda83ead455ac98597";
+const cities = [];
+const cityFormEl=document.querySelector("#city-search-form");
+const citySearchInputEl = document.querySelector("#searched-city");
+const  cityInputEl=document.querySelector("#city");
 
-var getCityWeather = function(city){
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+// make initial api call
+const getCityWeather = function(city){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(url)
     .then(function(response){
@@ -10,3 +15,16 @@ var getCityWeather = function(city){
         });
     });
 };
+
+// display the weather data from city search
+function displayWeatherData(weather){
+	console.log(json);
+  	const lat = json.coord.lat;
+  	const lon = json.coord.lon;
+  	const description = json.weather[0].description;
+  	const icon = json.weather[0].icon;
+  	const imageSRC = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  	const img = document.createElement("img");
+  	img.src = imageSRC;
+  	document.body.append(img);
+}
